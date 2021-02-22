5a9b94f134c730fb0c3f39cdb9e5bbb5d6bea663
AgolaDeveloper <owinoagola00@gmail.com>
2021-02-22 23:34:05 +0300

    adding object property as textContent

---
+++ main.js
@@ -0,0 +1,13 @@
+var person= {
+    fname: "OWINO",
+    sname: "AGOLA",
+    age: 23,
+    profession: "Software Developer",
+    salary: "2100 USD"
+};
+
+var nam= person.fname;
+document.getElementById("demo").innerHTML= nam.toLowerCase();
+//var n= person.Fname;
+
+//document.getElementById("p1").innerHTML=n;
